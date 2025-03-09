b33f975c248ad6997c22881e28319eb0a8aad958
Oliver Xu <liwenxu86@gmail.com>
2025-03-09 22:38:31 +1100

    ctags

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
+
+BASE=$(pwd)
+
+for rc in .*; do
+  mkdir -pv bak
+  [ -e ~/"$rc" ] && mv -v ~/"$rc" bak/"$rc"
+  ln -sfv "$BASE/$rc" ~/"$rc"
+done
+
+# git-prompt
+if [ ! -e ~/.git-prompt.sh ]; then
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+fi
+
+# scripts
+mkdir -p ~/bin
+for bin in $BASE/bin/*; do
+  ln -svf "$bin" ~/bin
+  
+done
+
+if [ "$(uname -s)" = 'Darwin' ]; then
+  # Homebrew
+  [ -z "$(which brew)" ] &&
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  brew tap universal-ctags/universal-ctags
+  brew install --HEAD universal-ctags
+
+  gem install gem-ctags
+  gem ctags
+else
+  rm -f ~/.tmux.conf
+  grep -v reattach-to-user-namespace tmux.conf > ~/.tmux.conf
+fi
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source-file ~/.tmux.conf
